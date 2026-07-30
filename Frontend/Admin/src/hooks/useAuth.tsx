8c6be8a8f9315563d41b283b5f670731c8ebc72b
AbiWD <abhilashbangera97@gmail.com>
2026-07-30 17:45:28 +0530

    Add abhilashbangera97@gmail.com to Admin superuser accounts

--- Frontend/Admin/src/hooks/useAuth.tsx
+++ Frontend/Admin/src/hooks/useAuth.tsx
@@ -72,7 +72,7 @@ export const useAuth = () => {
   const checkAdminStatus = (model: AuthModel) => {
     try {
       if (model) {
-        if (model.email === 'sysadmin@gamez.in' || model.email === 'admin@gamez.in' || model.role === 'admin' || model.email === 'test@admin.com') {
+        if (model.email === 'sysadmin@gamez.in' || model.email === 'admin@gamez.in' || model.email === 'abhilashbangera97@gmail.com' || model.role === 'admin' || model.email === 'test@admin.com') {
           setIsAdmin(true);
           setUserRole('admin');
         } else {
